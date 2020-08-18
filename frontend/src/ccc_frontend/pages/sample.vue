411b8b9ea8afc0569f48d8db2eae118724b7c3ce
myamano <nbz.orenge@gmail.com>
2020-08-18 13:14:40 +0900

    add: 本検索

---
+++ frontend/src/ccc_frontend/pages/sample.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="search">
+            <input type="text" v-model="keyword">
+            <button @click="getBooks">検索</button>
+        </div>
+        <div v-for="item in books.items" :key="item.id">
+            <div class="thumb">
+                <img v-if="item.volumeInfo.imageLinks" :src="item.volumeInfo.imageLinks.thumbnail" alt="">
+                <div class="no-photo" v-else>No Photo</div>
+            </div>
+            <div class="content">
+                <div class="title">{{item.volumeInfo.title}}</div>
+                <div class="page" v-if="item.volumeInfo.pageCount">{{item.volumeInfo.pageCount}}</div>
+                <div class="no-page" v-else>Page None</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="https://apis.google.com/js/api.js"></script>
+<script lang='ts'>
+import Vue from 'vue'
+
+const API_KEY = 'AIzaSyAy7O4MMFXlekjLQd7NQGg5qRau2aWCIGQ'
+const CALENDER_ID = 'nbz.orenge@gmail.com'
+
+export default Vue.extend({
+    data(){
+        return{
+            books:{},
+            keyword: "vue"
+        }
+    },
+    methods: {
+        getBooks(){
+            fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + this.keyword)
+            .then(res=>{
+                if(res.ok){
+                    return res.json();
+                }
+            })
+            .then(res=>{
+                this.books = res;
+                console.log(this.books)
+            })
+        },
+        start(){
+        }
+    },
+    mounted(){
+        this.getBooks()
+    }
+})
+</script>
+
+<style scoped>
+    .book{
+        display: flex;
+    }
+
+</style>
